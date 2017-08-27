d910764836ef626d8350ee5368e14af9d435b613
tacmac <tacuma.igei@gmail.com>
2017-08-27 21:02:22 +0900

    first commit

---
+++ src/main/java/com/zztwndemo/springboot/ZztwnDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zztwndemo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZztwnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZztwnDemoApplication.class, args);
+	}
+}
